8b0372f7324eaa97fa68b535647fb5b01943117f
Sweshika <sweshisj@gmail.com>
2025-06-24 07:40:03 +1000

    data_structures_eventjson

--- main.go
+++ main.go
@@ -1,6 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// events.json
+type Event struct {
+	EventType string          `json:"Event"`
+	Data      json.RawMessage `json:"Data"` // Use RawMessage to unmarshal later based on EventType
+}
+
+type ZoneDefinedData struct {
+	Name      string   `json:"Name"`
+	Postcodes []string `json:"Postcodes"`
+}
+
+type RateDefinedData struct {
+	ID        string  `json:"ID"`
+	MaxWeight float64 `json:"MaxWeight"`
+	Cost      float64 `json:"Cost"`
+	FromZone  string  `json:"FromZone"`
+	ToZone    string  `json:"ToZone"`
+}
 
 func main() {
 	fmt.Println("Hello, Flip!")
